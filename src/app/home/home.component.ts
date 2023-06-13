7854f6064421d2cb53c70ffc7d93a7e7918d430d
alfaromeo13 <jovanvukovic09@gmail.com>
2023-06-13 17:33:02 +0200

    commit.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,20 @@ import { NavbarLink } from "./navbar.model";
     styleUrls: ['./home.component.css']})
 export class HomeComponent implements OnInit{
     
+    picture : number = 0;
     isMobileNavActive :boolean = false;
     isUserAuthenticated :boolean = false;
+    bulmaCarousel = require('bulma-carousel/dist/js/bulma-carousel.min.js');
+    recived = false;
+    images = [""];
+    data = [
+        "https://images.unsplash.com/photo-1550921082-c282cdc432d6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+        "https://images.unsplash.com/photo-1550945771-515f118cef86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+        "https://images.unsplash.com/photo-1550971264-3f7e4a7bb349?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+        "https://images.unsplash.com/photo-1550931937-2dfd45a40da0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+        "https://images.unsplash.com/photo-1550930516-af8b8cc4f871?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+        "https://images.unsplash.com/photo-1550921082-c282cdc432d6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=8",
+    ]
 
     //we load navbar elements dynamicaly with ngFor
     links :NavbarLink[]=[
@@ -45,6 +57,17 @@ export class HomeComponent implements OnInit{
         this.authService.isAuthenticated.subscribe(data => {
             this.isUserAuthenticated = data;
         });
+        setTimeout(()=>{
+            this.images = this.data;
+          }, 100);
+        setTimeout(()=>{
+            this.bulmaCarousel.attach('#carousel', {
+              slidesToScroll: 3,
+              slidesToShow: 3,
+              autoplay: true,
+              loop: true
+            });
+          }, 1000);
     }
 
     toggleMobileView(): void {
--- src/app/testna-komponenta/test.component.ts
+++ src/app/testna-komponenta/test.component.ts
@@ -1,18 +1,37 @@
+import { AfterViewInit } from "@angular/core";
 import { Component } from "@angular/core";
-import { ToastrService } from "ngx-toastr";
 
 @Component({
     selector:'app-test',
     templateUrl:'test.component.html',
     styleUrls:['test.component.css'],
 })
-export class TestComponent{
-    inputValue :string="kkkkkk";   
-
-    constructor(private toastr:ToastrService){}
-
-    printVal() :void{
-            this.toastr.success('Successfully logged in!');
+export class TestComponent implements AfterViewInit{
+    bulmaCarousel = require('bulma-carousel/dist/js/bulma-carousel.min.js');
+    recived = false;
+    images = [""];
+    data = [
+      "https://img.freepik.com/free-vector/flame-abstract-logo_95982-235.jpg?size=626&ext=jpg",
+      "https://img.freepik.com/free-vector/mic-leaf-logo_83874-139.jpg?size=338&ext=jpg",
+      "https://img.freepik.com/free-vector/latter-e-fire-color_116762-8.jpg?size=338&ext=jpg",
+      "https://img.freepik.com/free-vector/fire-shield-logo_23758-270.jpg?size=338&ext=jpg",
+      "https://img.freepik.com/free-vector/fire-water-logo-design_149374-138.jpg?size=338&ext=jpg",
+      "https://img.freepik.com/free-vector/flame-abstract-logo_95982-235.jpg?size=626&ext=jpg"
+    ]
+   
+    constructor() {}
+  
+    ngAfterViewInit() {
+      setTimeout(()=>{
+        this.images = this.data;
+      }, 100);
+      setTimeout(()=>{
+        this.bulmaCarousel.attach('#carousel', {
+          slidesToScroll: 1,
+          slidesToShow: 4,
+          autoplay: true,
+          loop: true
+        });
+      }, 1000);
     }
-
 }
